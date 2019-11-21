dcf529f790e3a66ae423365bdad1560d497ae777
Tomy <tommy124593751@gamil.com>
2019-11-21 12:03:08 +0800

    feature: add new font

--- src/ui/themeProvider.tsx
+++ src/ui/themeProvider.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
-import {
-  MuiThemeProvider,
-  createMuiTheme,
-  responsiveFontSizes,
-} from '@material-ui/core/styles';
+import { MuiThemeProvider, createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import NotoSansTCRegularOtf from '../assets/font/NotoSansTC-Regular.otf';
+import NotoSansTCMediumOtf from '../assets/font/NotoSansTC-Medium.otf';
+import NotoSansTCBoldOtf from '../assets/font/NotoSansTC-Bold.otf';
+
+const NotoSansTCRegular = {
+  fontFamily: 'Noto Sans TC',
+  fontStyle: 'normal',
+  fontWeight: 400,
+  src: `
+    url(${NotoSansTCRegularOtf}) format('openType')
+  `,
+};
+const NotoSansTCMedium = {
+  fontFamily: 'Noto Sans TC',
+  fontStyle: 'normal',
+  fontWeight: 500,
+  src: `
+    url(${NotoSansTCMediumOtf}) format('openType')
+  `,
+};
+const NotoSansTCBold = {
+  fontFamily: 'Noto Sans TC',
+  fontStyle: 'normal',
+  fontWeight: 700,
+  src: `
+    url(${NotoSansTCBoldOtf}) format('openType')
+  `,
+};
 
 const appTheme = createMuiTheme({
   overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '@font-face': [NotoSansTCRegular, NotoSansTCMedium, NotoSansTCBold],
+      },
+    },
     MuiInputBase: {
       input: {
         fontSize: '1.4rem',
@@ -19,6 +48,7 @@ const appTheme = createMuiTheme({
     },
   },
   typography: {
+    fontFamily: ['"Noto Sans TC"', 'Roboto', 'Helvetica', 'Arial', 'sans-serif'].join(','),
     h1: {
       fontWeight: 'bolder',
     },
